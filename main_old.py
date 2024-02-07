6521b1be36c2b3acb868cb5b0a0b579a8e14dca7
Laurenz Gilbert <gilbert@uni-potsdam.de>
2024-02-07 10:15:26 +0100

    Added threading, seat removing

--- main_old.py
+++ main_old.py
@@ -3,6 +3,9 @@ import base64
 import json
 import time
 from io import BytesIO
+from zoneinfo import available_timezones
+import portalocker
+import threading
 
 import pytesseract
 import requests
@@ -23,6 +26,7 @@ product_id = 20123412
 event_id = 1977  # Bayern Spiel
 username = "freimut51371"
 password = "Nujbop-raxsor-8nyxqa"
+headers_path = "headers.json"
 
 # Es geht auch einfach nur nach event_id fetchen ohne block
 
@@ -35,6 +39,7 @@ blocks = [
     953, 954, 955, 956, 957, 958, 959, 960, 961, 962, 963, 964, 965, 966
 ]
 
+avilable_seats = {}
 
 # Session-Initialisierung
 session = tls_client.Session(
@@ -44,129 +49,173 @@ session = tls_client.Session(
 
 
 def fetch_new_headers():
-    # Konfiguriere Selenium WebDriver (Beispiel mit Chrome)
-    original_driver = Driver(uc=True)
 
-    try:
+    while True:
+        # Konfiguriere Selenium WebDriver (Beispiel mit Chrome)
+        original_driver = Driver(uc=True)
 
-        original_driver.get('https://www.bayer04.de/de-de/shop/tickets')
+        try:
 
-        # Wait for the page to load (you can use better waiting strategies)
-        original_driver.implicitly_wait(10)
+            original_driver.get('https://www.bayer04.de/de-de/shop/tickets')
 
-        # Check if an element with the class 'captcha-code' exists
-        captcha_elements = original_driver.find_elements(By.CLASS_NAME, 'captcha-code')
-        if captcha_elements:
-            # Extract the URL of the Captcha image
-            captcha_url = captcha_elements[0].get_attribute('src')
+
+            """
+            print("Loading cookies...")
+
+            try:
+                # Open the JSON file using a 'with' statement
+                with open("cookies.json", 'r') as json_file:
+                    # Read the contents of the JSON file
+                    json_data = json_file.read()
+
+                    # Check if the JSON data is not empty
+                    if json_data.strip():
+                        # Parse the JSON data
+                        cookies = json.loads(json_data)
+
+                        # Add each cookie to your driver
+                        for cookie in cookies:
+                            original_driver.add_cookie(cookie)
+
+                    else:
+                        print("The JSON file is empty.")
+            except FileNotFoundError:
+                print("The JSON file does not exist.")
+
+            print(f"Cookies: {original_driver.get_cookies()}")
+            """
+
+            original_driver.get('https://www.bayer04.de/de-de/shop/tickets')
+
+            # Wait for the page to load (you can use better waiting strategies)
+            original_driver.implicitly_wait(10)
+
+            # Check if an element with the class 'captcha-code' exists
+            captcha_elements = original_driver.find_elements(By.CLASS_NAME, 'captcha-code')
+
+            if captcha_elements:
+
+                try:
+                    # Extract the URL of the Captcha image
+                    captcha_url = captcha_elements[0].get_attribute('src')
+
+                    time.sleep(2)
+
+                    # Open a new WebDriver instance for fetching the image
+                    image_driver = webdriver.Chrome()
+
+                    # Go to the Captcha URL using the new WebDriver instance
+                    image_driver.get(captcha_url)
+
+                    time.sleep(5) # Ändern dass der auf Captcha image wartet
+
+                    # Fetch the Captcha image using the new WebDriver
+                    image_data = image_driver.find_element(By.TAG_NAME, 'img').get_attribute('src')
+
+                    image_bytes = base64.b64decode(image_data.split(',')[1])
+
+                    image = Image.open(BytesIO(image_bytes))
+
+                    # Use pytesseract to extract text from the image
+                    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+
+                    text = pytesseract.image_to_string(image).strip()
+
+                    if text:
+                        print("Found text in Captcha:", text)
+                    else:
+                        print("No captcha code found")
+                        original_driver.quit()
+                        image_driver.quit()
+                        continue
+
+                    # Close the new WebDriver instance
+                    image_driver.quit()
+
+                    # Explicitly wait for the input field to become available
+                    input_field = WebDriverWait(original_driver, 10).until(
+                        EC.presence_of_element_located((By.ID, "solution"))
+                    )
+
+                    # Send the characters one by one with a delay
+                    for char in text:
+                        input_field.send_keys(char)
+                        time.sleep(1)  # Adjust the delay as needed
+
+                    input_field.send_keys(Keys.RETURN)  # You can use Keys.RETURN to submit the input
+
+                    time.sleep(5)
+
+                    current_url = original_driver.current_url
+
+                    if current_url != "https://www.bayer04.de/de-de/shop/tickets":
+                        print("Didn't solve captcha")
+                        original_driver.quit()
+                        continue
+                
+                finally:
+                    # Close the new WebDriver instance even in case of exceptions
+                    image_driver.quit()
+
+            # Now, navigate to the login URL
+            original_driver.get(
+                'https://login.bayer04.de/login/?next=/authorize/%3Fclient_id%3Dq1pJ20GomJ6fjKxXee5HelQB4jhgKcAWjl9xHOH0%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fwww.bayer04.de%252Fde-de%252Fshop%252Fcustomer%252Flogin%252Fprofile%253Ftarget%253Dhome%26state%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDY3OTcwNDIsIm5iZiI6MTcwNjc5NzA0MiwiZXhwIjoxNzA2Nzk3OTQyfQ.qU5F5oNFw4a8eY8xwam0oOB44pwEZUKzKLfgpDjBizc')
+
+            # Find and enter the username slowly
+            username_input = WebDriverWait(original_driver, 10).until(
+                EC.presence_of_element_located((By.ID, "input-username"))
+            )
+
+            for char in username:
+                username_input.send_keys(char)
+                time.sleep(0.5)  # Adjust the delay as needed
+
+            # Find and enter the password
+            password_input = original_driver.find_element(By.ID, "input-password")
+
+            for char in password:  # Replace 'your_username' with your actual username
+                password_input.send_keys(char)
+                time.sleep(0.5)  # Adjust the delay as needed
+
+            # Find and click the login button
+            login_button = original_driver.find_element(By.ID, "signin-form-submit")
+            time.sleep(2)
+            login_button.click()
+
+            original_driver.get('https://www.bayer04.de/de-de/shop/customer/login/')
 
             time.sleep(2)
 
-            # Open a new WebDriver instance for fetching the image
-            image_driver = webdriver.Chrome()
+            original_driver.get('https://www.bayer04.de/de-de/shop/user/')
 
-            try:
-                # Go to the Captcha URL using the new WebDriver instance
-                image_driver.get(captcha_url)
+            time.sleep(2)
 
-                time.sleep(1)
+            pre_element = original_driver.find_element(By.TAG_NAME, "pre")
+            text = json.loads(pre_element.text)
+            auth_apf = text["accessToken"]
+            auth_tws = text["jwt"]
 
-                # Fetch the Captcha image using the new WebDriver
-                image_data = image_driver.find_element(By.TAG_NAME, 'img').get_attribute('src')
+            headers = {
+                'auth-apf': auth_apf,
+                'auth-tws': auth_tws,
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+            }
 
-                image_bytes = base64.b64decode(image_data.split(',')[1])
+            with open(headers_path, 'w') as file:
+                # Lock the file for writing
+                portalocker.lock(file, portalocker.LOCK_EX)
+                
+                # Write headers to the file in JSON format
+                json.dump(headers, file)
 
-                image = Image.open(BytesIO(image_bytes))
+            with open('cookies.json', 'w') as file:
+                json.dump(original_driver.get_cookies(), file, indent=2)  # Use json.dump() to write JSON data to the file
 
-                # Use pytesseract to extract text from the image
-                pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+            original_driver.quit()
 
-                text = pytesseract.image_to_string(image).strip()
-
-                if text:
-                    print("Found text in Captcha:", text)
-                else:
-                    print("No captcha code found")
-                    return
-
-                # Close the new WebDriver instance
-                image_driver.quit()
-
-                # Explicitly wait for the input field to become available
-                input_field = WebDriverWait(original_driver, 10).until(
-                    EC.presence_of_element_located((By.ID, "solution"))
-                )
-
-                # Send the characters one by one with a delay
-                for char in text:
-                    input_field.send_keys(char)
-                    time.sleep(1)  # Adjust the delay as needed
-
-                input_field.send_keys(Keys.RETURN)  # You can use Keys.RETURN to submit the input
-
-                time.sleep(5)
-
-                current_url = original_driver.current_url
-
-                if current_url != "https://www.bayer04.de/de-de/shop/tickets":
-                    print("Didn't solve captcha")
-                    original_driver.quit()
-                    return
-
-                # Now, navigate to the login URL
-                original_driver.get(
-                    'https://login.bayer04.de/login/?next=/authorize/%3Fclient_id%3Dq1pJ20GomJ6fjKxXee5HelQB4jhgKcAWjl9xHOH0%26response_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fwww.bayer04.de%252Fde-de%252Fshop%252Fcustomer%252Flogin%252Fprofile%253Ftarget%253Dhome%26state%3DeyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MDY3OTcwNDIsIm5iZiI6MTcwNjc5NzA0MiwiZXhwIjoxNzA2Nzk3OTQyfQ.qU5F5oNFw4a8eY8xwam0oOB44pwEZUKzKLfgpDjBizc')
-
-                # Find and enter the username slowly
-                username_input = WebDriverWait(original_driver, 10).until(
-                    EC.presence_of_element_located((By.ID, "input-username"))
-                )
-
-                for char in username:  # Replace 'your_username' with your actual username
-                    username_input.send_keys(char)
-                    time.sleep(0.5)  # Adjust the delay as needed
-
-                # Find and enter the password
-                password_input = original_driver.find_element(By.ID, "input-password")
-
-                for char in password:  # Replace 'your_username' with your actual username
-                    password_input.send_keys(char)
-                    time.sleep(0.5)  # Adjust the delay as needed
-
-                # Find and click the login button
-                login_button = original_driver.find_element(By.ID, "signin-form-submit")
-                time.sleep(2)
-                login_button.click()
-
-                original_driver.get('https://www.bayer04.de/de-de/shop/customer/login/?target=product_20123412')
-
-                time.sleep(2)
-
-                original_driver.get('https://www.bayer04.de/de-de/shop/user/')
-
-                time.sleep(2)
-
-                pre_element = original_driver.find_element(By.TAG_NAME, "pre")
-                text = json.loads(pre_element.text)
-                auth_apf = text["accessToken"]
-                auth_tws = text["jwt"]
-
-                headers = {
-                    'auth-apf': auth_apf,
-                    'auth-tws': auth_tws,
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-                }
-
-                return headers
-
-            finally:
-                # Close the new WebDriver instance even in case of exceptions
-                image_driver.quit()
-        else:
-            print("No Captcha code found.")
-    finally:
-        # Close the original WebDriver
-        original_driver.quit()
+        finally:
+            # Close the original WebDriver
+            original_driver.quit()
 
 
 # Funktionsdefinitionen
@@ -180,17 +229,17 @@ def send_telegram_message(bot_token, chat_id, message, parse_mode='Markdown'):
     print(response.text)
     return response.json()
 
+def remove_expired_seat_ids():
+    if avilable_seats:
+        current_time = time.time()
+        expired_seat_ids = [seat_id for seat_id, added_time in avilable_seats.items() if current_time - added_time > 900]  # 900 Sekunden sind 15 Minuten
+        for seat_id in expired_seat_ids:
+            del avilable_seats[seat_id]
 
 def check_seats():
     """
     Überprüft verfügbare Sitze für ein Event und benachrichtigt über Telegram.
     """
-
-    request_headers = {
-        'auth-apf': "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJCYXllcjA0IiwiZXhwIjoxNzA3MTYwNTQxLCJpYXQiOjE3MDcxMjQ1NDEsImN1c3RvbWVyX2dyb3VwcyI6IlNPTkRFUjIsU09OREVSMSxDTFVCLERLLU0yIiwic2NvcGUiOiIiLCJjb250YWN0X2lkIjoiMTk3YzBmZWEtY2UyMC00N2YxLTkwYTctOTg1MWEwMDI5YmVhIn0.zAPEKorONZHEyrpmUW4I2Ewn1JQyq5wfKZuRc9m-YzyfGHTwqsgeZ1FCTiUAw0FOCJ_ojfpnyic2ZY5SPeDRjhpxMKN-VUq0r25eF-HUEd7JvejwdE3xDGLNMCEXSjxAZlAZJcPEwwML867xvqN11oGZKO2JeERxtz7eXxvLIl_-Gho_Ktonq8dn-sPVOesAlD4877JcmHTdNv5c24u2CkEt2qFPq5HMmrrEfIT2WFg1H1hCSgXawUqSeRAmZTqsub5DnLhYUgzB3outDxXqdKS1djH8nnphzJlTZoibnSWDKVEWg4EVLxjOg4LZLdPLtJ4I4utSv0uVxUB6gIqbpEcQ3G_OCbKzK-5FWSLeXgKM9U7hFeQ8z0NSoVVkLb-atfIDml2hIFL1A3aYYRtixAVmYfNY8YeevmLWVBB_eBSeG3W0w-zV1iyLI4N-fznRLk0kMdqEhhi_i2Mu-eC731B6_Ay_1VczziHIkI-4o6cxeS1ultLs4XZAbRQmuUTSC-82pAQRGLxTxa9urW9a82NKi8XpuWF4SeXThJek2XCdFp8Zxy1JY7Mr5wMzuX_InxqFLVPzLlVlsZS68yFUpdu7HZyAmQ1LeKGb9gQZl5EYWBa0gdUINTG2nDX3_cN1V6TqESRdvecyfB2sqVApT8Pymj1VWvhhTsycV8qYbxo",
-        'auth-tws': "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpc3MiOiJ0d3MiLCJhdWQiOiJ1cm46Y3VzdG9tZXIiLCJpYXQiOjE3MDcxMjQ3MDYsImV4cCI6MTcwNzEyODMwNiwic2Vzc2lvbl9pZCI6ImJjMWE0MTc4NjkzZWYxYjM5ZDgzMDUzOTMyNDM1NyJ9.S49w9RMJjvPBv5mr20ot92x25mpWfmSzhgNCtqLxG4f5DvzF5mxtksR6sOYnXreeICnwYHOuoOfHzqt15TvTDVdijItsL29y4lDb1HMBR6bRh3Mr-A69uTe7W1OCzmthYM4SRjbjZpvVn304ZhcGqrIOZ7auICG8un5ygaChNksVPYIP95xalDI_D3BJ2leBZ7Rh9U0jwveXUduvPs0pBLknXVIWDrMyAa7cWvmRMJNZR727esfBAgFKg34XdfAB2sHRgFzBsII0ECcxXQ4C3xikcmfT42LGfIEnp4PuyuPpqzyCBN7x9Lyi-DRxBA_kgd5a9iHO8HDbaSRFDihv6g",
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-    }
 
     options_headers = {
         'authority': 'tss-al.bayer04.de',
@@ -208,6 +257,21 @@ def check_seats():
 
     while True:
         try:
+            print(f"Removing seats: {avilable_seats}")
+            remove_expired_seat_ids()
+            print(f"Removed seats: {avilable_seats}")
+
+            print("Fetching headers")
+
+            with open(headers_path, 'r') as file:
+                # Lock the file for reading (shared lock)
+                portalocker.lock(file, portalocker.LOCK_SH)
+                
+                # Read and return the headers from the file
+                request_headers = json.load(file)
+
+            print("Fetched headers")
+
             for block in blocks:
                 options_response = session.options(f'https://tss-al.bayer04.de/api/private/seats/{event_id}/{block}', headers=options_headers)
                 print(options_response.status_code)
@@ -222,6 +286,8 @@ def check_seats():
                     seat_ids = [seat["id"] for category in response_data[0]["category"] for seat in category["seats"]]
 
                     for seat_id in seat_ids:
+                        current_time = time.time()
+                        avilable_seats[seat_id] = current_time
                         print(f"Seat found for event {event_id} and block {block}. Seat_ID: {seat_id}")
                         message = f"Seat found for event {event_id} and block {block}. Seat\\_ID: {seat_id}\n [Checkout Link](https://www.bayer04.de/de-de/shop/product/{product_id})"
                         send_telegram_message(bot_token, chat_id, message, 'Markdown')
@@ -229,23 +295,9 @@ def check_seats():
                         
                 elif status_code == 502:
                     print(f"Status code: {status_code}")
-                    while True:
-                        headers = fetch_new_headers()
-                        if headers:
-                            request_headers = headers
-                            break
-                        else:
-                            continue
                         
                 elif status_code == 403:
                     print(f"Status code: {status_code}")
-                    while True:
-                        headers = fetch_new_headers()
-                        if headers:
-                            request_headers = headers
-                            break
-                        else:
-                            continue
 
                 else:
                     print(f"Unexpected status code: {status_code}")
@@ -262,4 +314,17 @@ def check_seats():
             continue
 
 
-check_seats()
+def main():
+    # Create threads
+    t1 = threading.Thread(target=check_seats)
+    t2 = threading.Thread(target=fetch_new_headers)
+
+    # Start threads
+    t1.start()
+    t2.start()
+
+    # Wait for threads to complete
+    t1.join()
+    t2.join()
+
+main()
